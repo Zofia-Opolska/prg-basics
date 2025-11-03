82752ba990a93e13885fe1e07c99b7216f3fab3a
Zofia <zosia.opolska@gmail.com>
2025-11-03 12:52:05 +0100

    dodaje zadania z lekcji

---
+++ test.py
@@ -0,0 +1,8 @@
+person = "John"
+print("Hello",person)
+print("Hello"+ person)
+print(f"Hello {person}")
+
+x = input("Enter data: ")
+#funkcja input zawsze zwraca tekst
+x = int(x)
---
+++ test2.py
@@ -0,0 +1,12 @@
+if 
+    instrukcja1
+    instrukcja2
+
+elif
+    instrukcja1
+    instrukcja2
+else:
+    instrukcja1
+    instrukcja2
+
+print('end')
