198252aea2701dacb5d2fe31ee02cadb0db345eb
Zofia <zosia.opolska@gmail.com>
2025-12-08 11:29:24 +0100

    2

--- 11-ObjectOrientedProgramming/socialmedia.py
+++ 11-ObjectOrientedProgramming/socialmedia.py
@@ -7,3 +7,19 @@ class SocialMediaProfile:
         self.posts.append(content)
         print(f"{self.username} added a new post: {content}")
 
+    def display_timeline(self):
+        print(self.username)
+        for p in self.posts:
+            print(p)
+
+username_1='johndoe'
+profil = SocialMediaProfile(username_1)
+content_1 = 'Hello, world!'
+content_2 = 'Had a great day at the park!'
+content_3 = "What's up, Natalie? How are you?"
+profil.add_post(content_1)
+profil.add_post(content_2)
+profil.add_post(content_3)
+profil.print_posts()
+
+    
