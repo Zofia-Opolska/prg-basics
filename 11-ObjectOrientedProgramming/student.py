06d0f39f47a5732667fe85af9dc54cda9c56f16c
Zofia <zosia.opolska@gmail.com>
2025-12-08 11:15:33 +0100

    2

--- 11-ObjectOrientedProgramming/student.py
+++ 11-ObjectOrientedProgramming/student.py
@@ -3,20 +3,26 @@ class Student():
     def __init__(self):
         self.name = ""
         self.age = 0
-
+        self.gender = ""
 def main():
     # object creation based on the class
     student1 = Student()
     student2 = Student()
+    student3 = Student()
     student1.name = "Dominic"
     student1.age = 19
+    student1.gender = "Male"
     student2.name = "Olivia"
     student2.age = 21
+    student2.gender = "Female"
+    student3.name = "Jhon"
+    student3.age = 20
+    student3.gender = "Male"
 
     print('LIST OF STUDENTS')
     print('================')
-    print(f'{student1.name}, {student1.age} years old')
-    print(f'{student2.name}, {student2.age} years old')
-
+    print(f'{student1.name}, {student1.age} years old, {student1.gender}')
+    print(f'{student2.name}, {student2.age} years old, {student2.gender}')
+    print(f'{student3.name}, {student3.age} years old, {student3.gender}')
 if __name__ == "__main__":
     main()
--- 11-ObjectOrientedProgramming/taxi.py
+++ 11-ObjectOrientedProgramming/taxi.py
@@ -1,6 +1,6 @@
 class TaxiRide:
     def __init__(self, rate_per_km):
-        self.rate_per_km = rate_per_km # value in € (e.g. €2)
+        self.rate_per_km = rate_per_km 
         self.distance = 0
         self.fare = 0
 
@@ -8,12 +8,36 @@ class TaxiRide:
         self.distance = distance
         self.fare = self.distance * self.rate_per_km
 
-
+    def receipt(self):
+        print('RECEIPT')
+        print('================')
+        print(f'Your fare is {self.fare}')
+        print(f'The distance was {self.distance}, and the rate was {self.rate_per_km}')
+        print('================')
 def main():
-    # your program
-    ...
-    ...
-    ...
+    driver_1=TaxiRide(4)
+    driver_2=TaxiRide(2)
+
+    driver_1.rate_per_km = 4
+    driver_1.distance = 100
+    driver_1.calculate_fare(driver_1.distance)
+    driver_2.rate_per_km = 2
+    driver_2.distance = 100
+    driver_2.calculate_fare(driver_2.distance)
+
+    driver_1.receipt()
+    driver_2.receipt()
+
+
+    '''print('RECEIPT')
+    print('================')
+    print(f'Your fare is {driver_1.fare}')
+    print(f'The distance was {driver_1.distance}, and the rate was {driver_1.rate_per_km}')
+    print('================')
+    print(f'Your fare is {driver_2.fare}')
+    print(f'The distance was {driver_2.distance}, and the rate was {driver_2.rate_per_km}')
+    print('================')'''
+
 
 if __name__ == "__main__":
     main()
