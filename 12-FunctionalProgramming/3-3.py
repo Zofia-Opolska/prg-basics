1edf4e51ba1be38d7c2db656f151a23acd4d0920
Zofia <zosia.opolska@gmail.com>
2025-12-08 12:57:25 +0100

    mam dosc

---
+++ 12-FunctionalProgramming/3-3.py
@@ -0,0 +1,7 @@
+# The data below contains a list of products available in stock and their unit price.
+
+stock = [(20,5.50),(15,8.30),(37,3.85),(4,11.60)]
+# Write a program that calculates the total value of products in stock. Use the map(), sum() and an anonymous function. Sample result:
+
+# Products in stock: [(20,5.50),(15,8.30),(37,3.85),(4,11.60)]
+# Total value: 423.35
