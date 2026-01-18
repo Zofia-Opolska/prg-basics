4f379d646bb4e4cdd05c5190901221462f48da03
Zofia Opolska <zosia.opolska@gmail.com>
2026-01-18 12:41:53 +0100

    kk

---
+++ TEST3-MOCK/p7.py
@@ -0,0 +1,15 @@
+#masz listę z nazwami i trzeba je posortować względem cyfr które są w tych nazwach 
+# def f(files):
+#     x=0
+#     arr=[]
+#     for i in files:
+#         for j in i:
+#             if j in [1,2,3,4,5,6,7,8,9,0]:
+            
+
+
+
+
+
+# files = ["copy179","copy15","copy3","copy123","copy9"]
+# print(f(files))
